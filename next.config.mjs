0a09008d7bcb34e9a5b34ddb6ca3e178e5bfe2cb
NestorGuzman182 <ing.nestor.guzman@gmail.com>
2024-02-08 10:17:39 -0500

    adding dinamic

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com', 'source.unsplash.com', 'plus.unsplash.com'],
-      },
-    async generateStaticParams(defaultPathMap) {
-      return {
-        '/': { page: '/' },
-        '/listado': { page: '/listado' },
-      }
-    }
+  images: {
+    domains: ['images.unsplash.com', 'source.unsplash.com', 'plus.unsplash.com'],
+  },
+  output: {
+    export: {}
+  },
+
+  async generateStaticParams() {
+    const staticParams = [
+      { params: { id: '1' } },
+      { params: { id: '2' } },
+    ];
+
+    return staticParams;
+  },
+
+
 };
 
 export default nextConfig;
+
