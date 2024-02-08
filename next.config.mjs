81d2bede09e7228219a3eb79b5da88d96e773e52
NestorGuzman182 <ing.nestor.guzman@gmail.com>
2024-02-08 17:17:13 -0500

    ajuste archivo next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'source.unsplash.com', 'plus.unsplash.com'],
   },
-  output: 'export'
-  ,
-
-  async generateStaticParams() {
-    const staticParams = [
-      { params: { id: '1' } },
-      { params: { id: '2' } },
-    ];
-
-    return staticParams;
-  },
-
-
+  output: 'export',
 };
 
 export default nextConfig;
-
